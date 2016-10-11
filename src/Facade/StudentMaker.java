b6e793921de11aba70d6441944c3c916e4b61445
aashgar <aashgar@yahoo.com>
2016-10-11 12:15:10 +0200

    new files have been added. programming is fun.

---
+++ src/Facade/StudentMaker.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Facade;
+
+/**
+ *
+ * @author aashgar
+ */
+public class StudentMaker {
+    private static StudentMaker aMaker;
+    private Student itStudent;
+    private Student engStudent;
+    private StudentMaker(){
+        this.itStudent = new ITStudent();
+        this.engStudent = new ENGStudent();
+    }
+    public static StudentMaker getMaker(){
+        if(aMaker == null)
+            aMaker = new StudentMaker();
+        return aMaker;
+    }
+    public void viewIT(){
+        this.itStudent.view();
+    }
+    public void viewENG(){
+        this.engStudent.view();
+    }
+    
+}
